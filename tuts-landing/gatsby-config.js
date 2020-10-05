a5a076c17870312e6bd4eb2c59930d60a19ddbc4
juliajcodes <john2122@purdue.edu>
2020-10-05 08:22:55 -0400

    added gatsby config

--- tuts-landing/gatsby-config.js
+++ tuts-landing/gatsby-config.js
@@ -5,6 +5,27 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  siteMetadata: {
+    title: `Juliacodes Front End Tutorials`,
+    description: `Juliacodes tutorials`,
+    author: `@juliacodes`,
+  },
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`, // This path is relative to the root of the site.
+        icon: `src/images/Icon.png`,
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 }
